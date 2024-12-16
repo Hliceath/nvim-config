1f15b2af7a0cab9010100771c215e480d3f831f1
Hliceath <hliceath@protonmail.com>
2024-12-16 15:14:31 +0100

    add godot and typescript formatters

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,10 +4,12 @@ return {
 	config = function()
 		require("conform").setup({
 			formatters_by_ft = {
+				gdscript = { "gdformat" },
 				lua = { "stylua" },
-				python = { "isort", "black" },
+				javascript = { "prettier" },
+				-- python = { "isort", "black" },
 				rust = { "rustfmt", lsp_format = "fallback" },
-				javascript = { "prettierd", "prettier", stop_after_first = true },
+				typescript = { "prettier" },
 			},
 			format_on_save = {
 				timeout_ms = 500,
