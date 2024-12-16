770598c92611f7960f70ce3e520e604c8c751f85
Hliceath <hliceath@protonmail.com>
2024-12-16 23:55:17 +0100

    Remove error lsp and add highlight for gdscript

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -5,7 +5,7 @@ return {
 		local configs = require("nvim-treesitter.configs")
 
 		configs.setup({
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "html", "rust", "typescript" },
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "html", "rust", "typescript", "gdscript", "godot_resource", "gdshader" },
 			sync_install = false,
 			highlight = { enable = true },
 			indent = { enable = true },
